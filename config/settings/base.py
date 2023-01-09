fa466b4f2fa9e22ad3e20992c57c9176ba86abfe
Michal Stachura <michal.stachura@santri.pl>
2023-01-09 16:04:54 +0100

    Add base app

--- config/settings/base.py
+++ config/settings/base.py
@@ -79,6 +79,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "hades_star_backend.users",
     # Your stuff: custom apps go here
+    "hades_star_backend.corporations",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
